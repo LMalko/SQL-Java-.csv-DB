7ba96a2d92a34420b82d43ed5199818f600c7adc
LMalko86 <codewars@onet.pl>
2018-02-19 10:56:01 +0100

    Change package

---
+++ src/Java/Main.java
@@ -0,0 +1,11 @@
+package Java;
+
+import App.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application start = new Application();
+        start.startApp();
+    }
+}
